d0ae265df6e5d600dbee604afd5bda5708c06bcd
ZIUGOD <caique.dubstep@gmail.com>
2023-09-08 03:31:21 -0300

    Restarting...

---
+++ ziu/src/components/title/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react";
+
+export default function Title() {
+    return (
+        <view>
+            <Text>ZIU</Text>
+        </view>
+    );
+}
