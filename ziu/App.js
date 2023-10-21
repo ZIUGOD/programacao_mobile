6a23707bba5b2e2604ef488efa5da690adf19073
ZIUGOD <caique.dubstep@gmail.com>
2023-10-21 15:24:23 -0300

    att

--- ziu/App.js
+++ ziu/App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Title from "./src/components/title/";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Title />
     </View >
   );
 }
